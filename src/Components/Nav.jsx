ae94d0398b7a0bc730f1193841edc3f73a739eac
sriram0808 <sriram08082000@gmail.com>
2024-04-09 10:10:02 +0530

    code updated

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -4,25 +4,21 @@ import logo from "../Components/Assest/logo3.png";
 import { Link } from 'react-router-dom';
 import { FaFacebook, FaInstagram, FaTwitter, FaLinkedin } from 'react-icons/fa';
 
-
-
 let Nav = () => {
     return (
         <div>
-            <div className="container-fluid " style={{ boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)"}}>
-                <div className={`${style.navBar} row align-items-center `}>
+            <div className="container-fluid" style={{ boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)"}}>
+                <div className={`${style.navBar} row align-items-center`}>
                     <div className="col-auto">
-                        <img src={logo} alt="" className={`${style.logo}  float-start`} />
-          
-           <div className="container-fluid sticky-top " style={{ boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)" }}>
-                <button className={`btn btn-primary  position-fixed top-0 end-0 m-5 mt-4 ${style.stickyToggle} ${style.roundedButton}`} type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">
-                <BiMenu />
-            </button>
+                        <img src={logo} alt="" className={`${style.logo} float-start`} />
+                    </div>
+                    <div className="col">
+                        <button className={`btn btn-primary position-fixed top-0 end-0 m-5 mt-4 ${style.stickyToggle} ${style.roundedButton}`} type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">
+                            <BiMenu />
+                        </button>
+                    </div>
+                </div>
             </div>
-
-           
-           </div>
-          
 
             <div className="position-relative">
                 <div className="offcanvas offcanvas-end" tabIndex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
@@ -35,26 +31,21 @@ let Nav = () => {
                             <li><Link to="/" className={style.navLink}>Home</Link></li>
                             <li><Link to="/about" className={style.navLink}>About</Link></li>
                             <li><Link to="/service" className={style.navLink}>Service</Link></li>
-                            <li><Link to="/carrers" className={style.navLink}>Carrers</Link></li>
-                            <li><Link to="/contactus" className={style.navLink}>Contactus</Link></li>
+                            <li><Link to="/careers" className={style.navLink}>Careers</Link></li>
+                            <li><Link to="/contactus" className={style.navLink}>Contact Us</Link></li>
                         </ol>
-                        <div className="container ">
-                            <div className="row justify-content-between ">
-                            <div className="col"><a href="/" style={{ color: "white" }}><FaFacebook/> </a></div>
-                            <div className="col"><a href="/" style={{ color: "white" }}><FaInstagram/> </a></div>
-                            <div className="col"><a href="/" style={{ color: "white" }}><FaTwitter/> </a></div>
-                            <div className="col"><a href="/" style={{ color: "white" }}><FaLinkedin/> </a></div>
-                             </div>
+                        <div className="container">
+                            <div className="row justify-content-between">
+                                <div className="col"><a href="/" style={{ color: "white" }}><FaFacebook/> </a></div>
+                                <div className="col"><a href="/" style={{ color: "white" }}><FaInstagram/> </a></div>
+                                <div className="col"><a href="/" style={{ color: "white" }}><FaTwitter/> </a></div>
+                                <div className="col"><a href="/" style={{ color: "white" }}><FaLinkedin/> </a></div>
+                            </div>
                         </div>
                     </div>
-                   
-                   
                 </div>
             </div>
         </div>
-        </div>
-        </div>
-    
     );
 }
 
