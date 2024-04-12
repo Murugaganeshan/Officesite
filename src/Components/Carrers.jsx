45d3398061fa5d1aa06f67c4ec4283bd595e7e59
Murugaganeshan <murugaganeshan18@gmail.com>
2024-04-12 10:46:41 +0530

    pull

--- src/Components/Carrers.jsx
+++ src/Components/Carrers.jsx
@@ -1,5 +1,11 @@
 import Inter from "../Assets/Images/inter.jpg"
 import "../Assets/Css/career.css"
+import Accordion from '@mui/material/Accordion';
+import AccordionActions from '@mui/material/AccordionActions';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import Button from '@mui/material/Button';
 import {useRef} from 'react';
 let Carrers=()=>{
     const ref = useRef(null);
@@ -40,140 +46,160 @@ let Carrers=()=>{
            </h1>
 
           
-
-<div className="container-xl my-4" >
-        
-        <div className="row row-cols-lg-3 row-cols-md-2 row-cols-sm-1  row-cols-1 d-flex justify-content-center my-3">
-        
-         
-        <div  className="col pb-4 my-3">
-                <div class="card-shop card ">
-               <h2>Business Development Executive</h2>
-                  
-                    <ul>
+            <div className="container p-3">
+            <div className="row row-cols-lg-2 row-cols-md-1 row-cols-sm-1 row-cols-1 justify-content-center">
+              <div className="col"> 
+      
+      
+      <Accordion >
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel3-content"
+          id="panel3-header"
+        >
+         <h5>Business Development Executive</h5>
+        </AccordionSummary>
+        <AccordionDetails>
+          <h6>Experiece: 0-1 Year</h6>
+          <h6>Skills</h6>
+        <ul>
         <li>Strong communication skills, both verbal and written, are essential. You should be able to effectively communicate with clients, colleagues, and other stakeholders. </li>
         <li>Additionally, having basic sales skills and the ability to pitch products or services persuasively is important.</li>
         <li>Fluently in English.</li>
         <li> Employers value candidates who are adaptable and willing to learn new technologies. Highlight any experiences where you quickly picked up new skills or adapted to new challenges..</li>
     </ul> 
-    
+        </AccordionDetails>
+        <AccordionActions>
+        <div  className="mx-3 d-flex justify-content-start btn_pre p-2">  
                    
-                    <div className="  col ">
-                    <div  className="mx-3 d-flex justify-content-start btn_pre">  
-                   
-              <a href="/apply"> <button  type="button" class="btn-preview "> Apply </button></a> 
+                   <a href="/apply"> <button  type="button" class="btn-preview "> Apply </button></a> 
 
-                   </div>
-                    </div>
-                 
-                  
-                
-                </div>
-              </div>
-
-              <div  className="col pb-4 my-3">
-                <div class="card-shop card ">
-               <h2>Front End Developer</h2>
-                  
-                    <ul>
+                  </div>
+        </AccordionActions>
+      </Accordion>
+    </div>
+    <div className="col"> 
+      
+      
+      <Accordion >
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel3-content"
+          id="panel3-header"
+        >
+      <h5>Front End Developer</h5>
+        </AccordionSummary>
+        <AccordionDetails>
+        <h6>Experiece: 0-1 Year</h6>
+          <h6>Skills</h6>
+          <ul>
         <li>Proficiency in HTML, CSS, and JavaScript is essential. These are the core technologies used in front-end development.</li>
         <li>Understanding of responsive design principles and experience with CSS preprocessors like Sass or Less.</li>
+        <li>Expertise in React and related libraries for building modern web applications</li>
+        <li>Ability to write well-documented, clean JavaScript code.</li>
         <li>Basic understanding of back-end technologies and how they interact with the front end. This might include knowledge of RESTful APIs, server-side rendering, or basic database operations.</li>
         <li>Knowledge of front-end frameworks and libraries such as React.js. Familiarity with at least one of these frameworks is often required.</li>
     </ul>
-   
+        </AccordionDetails>
+        <AccordionActions>
+        <div  className="mx-3 d-flex justify-content-start btn_pre p-2">  
                    
-                    <div className="  col ">
-                    <div  className="mx-3 d-flex justify-content-start btn_pre">  
-                   
-                    <a href="/apply"> <button  type="button" class="btn-preview "> Apply </button></a> 
+                   <a href="/apply"> <button  type="button" class="btn-preview "> Apply </button></a> 
 
-                   </div>
-                    </div>
-                 
-                  
-                
-                </div>
-              </div>
-
-              <div  className="col pb-4 my-3">
-                <div class="card-shop card ">
-               <h2>Business Development Executive</h2>
-                  
-                    <ul>
-        <li>Lorem gfdgdfgipsumdfsdfdsffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff</li>
-        <li>LofdsfsdfsdfsLofdsfsdfsdfsdrem ipsumLofdsfsdfsdfsdrem ipsumLofdsfsdfsdfsdrem ipsumLofdsfsdfsdfsdrem ipsumdrem ipsum</li>
-        <li>Lorem ipsum</li>
-        <li>Lorem ipsum</li>
-    </ul>
-   
-                   
-                    <div className="  col ">
-                    <div  className="mx-3 d-flex justify-content-start btn_pre">  
-                   
-                    <a href="/apply"> <button  type="button" class="btn-preview "> Apply </button></a> 
-
-                   </div>
-                    </div>
-                 
-                  
-                
-                </div>
-              </div>
-              <div  className="col pb-4 my-3">
-                <div class="card-shop card ">
-               <h2>Business Development Executive</h2>
-                  
-                    <ul>
-        <li>Lorem gfdgdfgipsumdfsdfdsffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff</li>
-        <li>LofdsfsdfsdfsLofdsfsdfsdfsdrem ipsumLofdsfsdfsdfsdrem ipsumLofdsfsdfsdfsdrem ipsumLofdsfsdfsdfsdrem ipsumdrem ipsum</li>
-        <li>Lorem ipsum</li>
-        <li>Lorem ipsum</li>
-    </ul>
-   
-                   
-                    <div className="  col ">
-                    <div  className="mx-3 d-flex justify-content-start btn_pre">  
-                    <a href="/apply"> <button  type="button" class="btn-preview "> Apply </button></a> 
-
-                   </div>
-                    </div>
-                 
-                  
-                
-                </div>
-              </div>
-              <div  className="col pb-4 my-3">
-                <div class="card-shop card ">
-               <h2>Business Development Executive</h2>
-                  
-                    <ul>
-        <li>Lorem gfdgdfgipsumdfsdfdsffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff</li>
-        <li>LofdsfsdfsdfsLofdsfsdfsdfsdrem ipsumLofdsfsdfsdfsdrem ipsumLofdsfsdfsdfsdrem ipsumLofdsfsdfsdfsdrem ipsumdrem ipsum</li>
-        <li>Lorem ipsum</li>
-        <li>Lorem ipsum</li>
-    </ul>
-   
-                   
-                    <div className="  col ">
-                    <div  className="mx-3 d-flex justify-content-start btn_pre">  
-                   
-                    <a href="/apply"> <button  type="button" class="btn-preview "> Apply </button></a> 
-
-                   </div>
-                    </div>
-                 
-                  
-                
-                </div>
-              </div>
-           
-              </div>
-             
-          
+                  </div>
+        </AccordionActions>
+      </Accordion>
+    </div>
+    <div className="col my-4"> 
       
+      
+      <Accordion >
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel3-content"
+          id="panel3-header"
+        >
+           <h5>Back End Developer</h5>
+        </AccordionSummary>
+        <AccordionDetails>
+        <h6>Experiece: 0-1 Year</h6>
+          <h6>Skills</h6>
+          <ul>
+        <li>Good knowledge of Node.js with 1 year of minimum experience.</li>
+        <li>Good exposure on Mongodb and Express.js.</li>
+        <li>User authentication and authorization between multiple systems, servers, and environments.</li>
+        <li>Integration of multiple data sources and databases into one system.</li>
+        <li>Proficient understanding of code versioning tools, such as GitLab, GitHub, Bitbucket.</li>
+         <li>Basic understanding of front-end technologies, such as HTML5, Bootstrap, and CSS3.</li>
+    </ul>
+        </AccordionDetails>
+        <AccordionActions>
+          <Button>Cancel</Button>
+          <Button>Agree</Button>
+        </AccordionActions>
+      </Accordion>
+    </div>
+    <div className="col my-4"> 
+      
+      
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel3-content"
+          id="panel3-header"
+        >
+           <h5>Back End Developer</h5>
+        </AccordionSummary>
+        <AccordionDetails>
+        <h6>Experiece: 0-1 Year</h6>
+          <h6>Skills</h6>
+          <ul>
+        <li>Good knowledge of Node.js with 1 year of minimum experience.</li>
+        <li>Good exposure on Mongodb and Express.js.</li>
+        <li>User authentication and authorization between multiple systems, servers, and environments.</li>
+        <li>Integration of multiple data sources and databases into one system.</li>
+        <li>Proficient understanding of code versioning tools, such as GitLab, GitHub, Bitbucket.</li>
+         <li>Basic understanding of front-end technologies, such as HTML5, Bootstrap, and CSS3.</li>
+    </ul>
+        </AccordionDetails>
+        <AccordionActions>
+          <Button>Cancel</Button>
+          <Button>Agree</Button>
+        </AccordionActions>
+      </Accordion>
+    </div>
+    <div className="col my-4"> 
+      
+      
+      <Accordion >
+        <AccordionSummary
+         expandIcon={<ExpandMoreIcon />}
+         aria-controls="panel1-content"
+         id="panel1-header"
+        >
+           <h5>Back End Developer</h5>
+        </AccordionSummary>
+        <AccordionDetails>
+        <h6>Experiece: 0-1 Year</h6>
+          <h6>Skills</h6>
+          <ul>
+        <li>Good knowledge of Node.js with 1 year of minimum experience.</li>
+        <li>Good exposure on Mongodb and Express.js.</li>
+        <li>User authentication and authorization between multiple systems, servers, and environments.</li>
+        <li>Integration of multiple data sources and databases into one system.</li>
+        <li>Proficient understanding of code versioning tools, such as GitLab, GitHub, Bitbucket.</li>
+         <li>Basic understanding of front-end technologies, such as HTML5, Bootstrap, and CSS3.</li>
+    </ul>
+        </AccordionDetails>
+        <AccordionActions>
+          <Button>Cancel</Button>
+          <Button>Agree</Button>
+        </AccordionActions>
+      </Accordion>
+    </div>
+    </div>
 
-      </div>
+            </div>
         </div>
     )
 }
