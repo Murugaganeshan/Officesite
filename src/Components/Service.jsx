99a1311d14d7215cefa475db70d6c8673c02caab
sriram0808 <sriram08082000@gmail.com>
2024-04-17 17:21:54 +0530

    by sri

--- src/Components/Service.jsx
+++ src/Components/Service.jsx
@@ -14,8 +14,8 @@ let Service=()=>{
 
     return(
 
-        <div style={{backgroundColor:"lightblue"}}>
-                <section class="py-5 py-xl-8">
+        <div>
+                <section class="py-5 py-xl-8" style={{backgroundColor:"lightblue"}}>
                 <div class="container">
                     <div class="row justify-content-md-center">
                     <div class="col-12 col-md-10 col-lg-8 col-xl-7 col-xxl-6">
@@ -114,7 +114,7 @@ let Service=()=>{
                             <p class="mb-3 text-secondary">Pioneering AI solutions tailored to your business needs. Leveraging advanced algorithms and machine learning techniques, we empower organizations to automate processes, gain insights, and optimize operations. From predictive analytics to natural language processing, we deliver cutting-edge AI technologies for impactful results.</p>
                             <div>
                             <a href="#!" class="fw-bold text-decoration-none link-primary">
-                                <Link to="aidevelopment">Learn More</Link>
+                                <Link to="/aidevelopment">Learn More</Link>
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-short" viewBox="0 0 16 16">
                                 <path fill-rule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z" />
                                 </svg>
