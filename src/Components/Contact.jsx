e71dd30964a87a5c6755ebc878addb63075d5d40
sriram0808 <sriram08082000@gmail.com>
2024-06-12 16:11:32 +0530

    bysri

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -97,7 +97,7 @@ let Contact = () => {
                 <div class="container">
                     <div class="row">
                         <div class="   text-center contactpage pb-3" data-aos="fade-in">
-                            <h3 class="fs-5 mb-2  contactTitle text-uppercase ">Contact Us</h3>
+                            <h3 class="fs-5 mb-2  contactTitle  ">Contact Us</h3>
                             <h2 class="display-5 mb-4 mb-md-5 mb-xl-8 contact contactSubTitle" >We're always on the lookout to work with new clients. Please get in touch in one of the following ways.</h2>
                         </div>
                     </div>
