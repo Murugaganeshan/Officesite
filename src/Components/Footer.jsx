70b735c65fb1cf1da291c78ff5e081f67d1a1f9a
sriram0808 <sriram08082000@gmail.com>
2024-06-12 15:38:19 +0530

    bysri

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,11 +1,15 @@
-import { Link } from "react-router-dom";
-import { GoArrowRight } from "react-icons/go";
 import logo from "../Assets/Images/finalLogo.jpg"
 import { FaLinkedin } from "react-icons/fa";
 import { FaFacebook, FaInstagram, FaTwitter, FaWhatsapp } from 'react-icons/fa';
-import { RiWhatsappFill } from "react-icons/ri";
 import "../Assets/Css/Footer.css" 
 import { useState } from "react";
+import arrow from "../Assets/Images/arrow.png"
+import copyright from "../Assets/Images/copyright.png"
+import faceBook from "../Assets/Images/faceBook.png"
+import insta from "../Assets/Images/insta.png"
+import twitter from "../Assets/Images/twitter.png"
+import whatsapp from "../Assets/Images/whatsapp.png"
+import linkedin from "../Assets/Images/linkedin.png"
 
 let Footer=()=>{
   const closeMenu=()=> setClick(false)
@@ -16,7 +20,7 @@ let Footer=()=>{
     <div className='footer container-fluid'>
 
       
-      <div className="row">
+      <div className="row footerBack">
         <div class='col-12 col-lg-4  py-4 py-md-5 py-xxl-8   footerBlock'>
           <div class="row h-70 align-items-end justify-content-center">
             <div class="col-12 col-md-11 col-xl-10 footer-con">
@@ -29,21 +33,21 @@ let Footer=()=>{
                 <h1> Follow Us </h1>
                     <div className="social-links my-4">
                       <div className="social-div">
-                       <a href="/"><FaFacebook/> </a> <h6>Facebook</h6>
+                       <a href="/"><img src={faceBook} alt="" className="img-fluid" style={{height:"46px"}}/> </a> <h6>Facebook</h6>
                        </div>
                        <div className="social-div">
-                       <a href="/"><FaInstagram/>  </a><h6>Instagram</h6>
+                       <a href="/"><img src={insta} alt="" className="img-fluid" style={{height:"46px"}}/>  </a><h6>Instagram</h6>
 
                        </div>
                        <div className="social-div">
-                       <a href="/"><FaTwitter/> </a><h6>Twitter</h6>
+                       <a href="/"><img src={twitter} alt="" className="img-fluid" style={{height:"46px"}}/> </a><h6>Twitter</h6>
                        </div>
                        <div className="social-div">
-                       <a href="/"><FaWhatsapp/> </a><h6>Whatsapp</h6>
-                       </div>
-                    
-                      
-                       <a href="https://www.linkedin.com/company/101978309/admin/inbox/thread/2-NmRmNTc4NWUtZGRiZS00Y2IwLTgwNDktNWExNmNiMTNkODc0XzAxMA==/"><FaLinkedin/></a>
+                       <a href="/"><img src={whatsapp} alt="" className="img-fluid" style={{height:"46px"}}/> </a><h6>Whatsapp</h6>
+                       </div>   
+                       <div className="social-div">
+                       <a href="https://www.linkedin.com/company/101978309/admin/inbox/thread/2-NmRmNTc4NWUtZGRiZS00Y2IwLTgwNDktNWExNmNiMTNkODc0XzAxMA==/"><img src={linkedin} alt="" className="img-fluid" style={{height:"46px"}}/> </a><h6>Linkedin</h6>
+                       </div>                   
                     </div>
                 </div>
                     
@@ -59,23 +63,23 @@ let Footer=()=>{
                     <h4 class="widget-title mb-4 footerTitle">Services</h4>
                     <ul class="list-unstyled service">
                       <li class="mb-3">
-                      <a href="/mobileapp"  class="link-secondary text-decoration-none footerSubTitle"><GoArrowRight /> Mobile Development </a>
+                      <a href="/mobileapp"  class="link-secondary text-decoration-none footerSubTitle"><img src={arrow} alt="" className="img-fluid"/> Mobile Development </a>
                       </li>
                       <li class="mb-3">
-                      <a href="/webdevelopment"  class="link-secondary text-decoration-none footerSubTitle"><GoArrowRight /> Web Development </a>
+                      <a href="/webdevelopment"  class="link-secondary text-decoration-none footerSubTitle"><img src={arrow} alt="" className="img-fluid"/> Web Development </a>
                       </li>
                       <li class="mb-3">
-                      <a href="/uiux"  class="link-secondary text-decoration-none footerSubTitle"><GoArrowRight /> UI/UX Development </a>
+                      <a href="/uiux"  class="link-secondary text-decoration-none footerSubTitle"><img src={arrow} alt="" className="img-fluid"/> UI/UX Development </a>
                       </li>
                       <li class="mb-3">
-                      <a href="/aidevelopment"  class="link-secondary text-decoration-none footerSubTitle"><GoArrowRight /> AI Development </a>
+                      <a href="/aidevelopment"  class="link-secondary text-decoration-none footerSubTitle"><img src={arrow} alt="" className="img-fluid"/> AI Development </a>
 
                       </li>
                       <li class="mb-3">
-                      <a href="/cripto"  class="link-secondary text-decoration-none footerSubTitle"><GoArrowRight /> Crypto Trading </a>
+                      <a href="/cripto"  class="link-secondary text-decoration-none footerSubTitle"><img src={arrow} alt="" className="img-fluid"/> Crypto Trading </a>
                       </li>
                       <li class="mb-3">
-                        <a href="/neft"  class="link-secondary text-decoration-none footerSubTitle"><GoArrowRight /> NFT Marketing </a>
+                        <a href="/neft"  class="link-secondary text-decoration-none footerSubTitle"><img src={arrow} alt="" className="img-fluid"/> NFT Marketing </a>
                       </li>
                     </ul>
                   </div>
@@ -88,33 +92,33 @@ let Footer=()=>{
          
 
 
-              <a href="/"  class="link-secondary text-decoration-none footerSubTitle"><GoArrowRight /> Home</a>
+              <a href="/"  class="link-secondary text-decoration-none footerSubTitle"><img src={arrow} alt="" className="img-fluid"/> Home</a>
             </li>
            
             <li className="mb-3">
           
               
-              <a href="/about"  class="link-secondary text-decoration-none footerSubTitle"><GoArrowRight /> About</a>
+              <a href="/about"  class="link-secondary text-decoration-none footerSubTitle"><img src={arrow} alt="" className="img-fluid"/> About</a>
             </li>
             <li className="mb-3">
           
-               <a href="/service"  class="link-secondary text-decoration-none footerSubTitle"><GoArrowRight /> Service</a>
+               <a href="/service"  class="link-secondary text-decoration-none footerSubTitle"><img src={arrow} alt="" className="img-fluid"/> Service</a>
             </li>
             <li className="mb-3">
           
-               <a href="/career"  class="link-secondary text-decoration-none footerSubTitle"><GoArrowRight /> Career</a>
+               <a href="/career"  class="link-secondary text-decoration-none footerSubTitle"><img src={arrow} alt="" className="img-fluid"/> Career</a>
             </li>
             <li className="mb-3">
         
-               <a href="/contact"  class="link-secondary text-decoration-none footerSubTitle"><GoArrowRight /> Contact</a>
+               <a href="/contact"  class="link-secondary text-decoration-none footerSubTitle"><img src={arrow} alt="" className="img-fluid"/> Contact</a>
             </li>
             <li className="mb-3">
 
-            <a href="/contact"  class="link-secondary text-decoration-none footerSubTitle"><GoArrowRight /> Terms & Condition</a>
+            <a href="/contact"  class="link-secondary text-decoration-none footerSubTitle"><img src={arrow} alt="" className="img-fluid"/> Terms & Condition</a>
             </li>
             <li className="mb-3">
 
-            <a href="/contact"  class="link-secondary text-decoration-none footerSubTitle"><GoArrowRight /> Privacy Policy</a>
+            <a href="/contact"  class="link-secondary text-decoration-none footerSubTitle"><img src={arrow} alt="" className="img-fluid"/> Privacy Policy</a>
             </li>
 
 
@@ -133,12 +137,12 @@ let Footer=()=>{
                         <div class="col-12">
                           <div class="input-group">
                            
-                            <input type="email" class="form-control" id="email-newsletter"  placeholder="email" aria-label="email-newsletter" aria-describedby="email-newsletter-addon" required />
+                            <input type="email" class="form-control subscribeBack" id="email-newsletter"  placeholder="Email" aria-label="email-newsletter" aria-describedby="email-newsletter-addon" required />
                           </div>
                         </div>
                         <div class="col-12">
                           <div class="d-grid">
-                            <button class="btn btn-primary" type="submit">Subscribe</button>
+                            <button class="btn subscribeButton" type="submit">Subscribe</button>
                           </div>
                         </div>
                       </div>
@@ -149,7 +153,7 @@ let Footer=()=>{
               
               <div class="row mt-6 border-top border-light-subtle my-5 text-start">
                 <div class="footer-copyright-wrapper foot my-4">
-                  &copy; copyrights <span className="fw-bold companyName">Thamizhan Solutions</span> All Rights Reserved.
+                 <img src={copyright} alt="" className="img-fluid" style={{height:"20px"}}/> copyrights <span className="fw-bold companyName">Thamizhan Solutions</span> All Rights Reserved.
                 </div>        
               </div>
             </div>
